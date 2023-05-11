12874cfdb7dbd6972752726f42cc8ec2e6dc1fb1
Maimuna Zaheer <68902873+itserror404@users.noreply.github.com>
2023-05-11 05:55:54 +0400

    Add files via upload

---
+++ MaimunaZaheerLab18/contact.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import cgi
+
+
+# get form data
+form = cgi.FieldStorage()
+name = form.getvalue('name')
+email = form.getvalue('email')
+compalin = form.getvalue('complain')
+accept = form.getvalue('accept')
+
+# create HTML response
+print("""Content-type: text/html /n 
+    <html> 
+    <head> 
+    <title>Contact Form Response</title> 
+    <link rel="stylesheet" href="style.css"></head><body>""")
+
+print("""<h1>Contact Form Response</h1>""")
+print ("<p><b>Name:</b>" + name +"</p>")
+print("<p><b>Email:</b>"+email"</p>")
+
+print("<p><b>Message:</b>"+ message+ "</p>")
+
+if subscribe:
+    print("<p><b>Accepted terms and condtions</b>Yes</p>")
+else:
+    print("<p><b>Accepted terms and condtions</b> No</p>")
+print("</body>")
+print("</html>")
